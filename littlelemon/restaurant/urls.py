395fb694a80ecfc5b41b78a16d474a4ef1f81d91
jeremyhatcher <113803413+jeremyhatcher@users.noreply.github.com>
2023-03-02 20:25:25 -0800

    added APIs

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet, basename= 'booking')
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('', include(router.urls)),
 ]
